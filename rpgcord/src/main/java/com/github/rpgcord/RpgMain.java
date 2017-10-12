5a7a6aa8d791343fa069792be939b719011f8a04
Alan Drieux <32481840+alan-drieux@users.noreply.github.com>
2017-10-12 10:04:55 +0200

    Add files via upload

---
+++ rpgcord/src/main/java/com/github/rpgcord/RpgMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.rpgcord;
+
+/**
+ *
+ * @author alan
+ */
+public class RpgMain {
+    public static void RpgMain(String[] args){
+        
+    }
+    
+}
